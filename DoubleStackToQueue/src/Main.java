bc7efd85a76f6147b5fa8f1e4f23895def62a49a
wenjian <wenjian@onos6-134.edu.cn>
2016-09-09 22:13:56 +0800

    add double stack to implement queue

---
+++ DoubleStackToQueue/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution s = new Solution();
+		s.push(1);
+		s.push(2);
+		s.push(3);
+		System.out.print(s.pop()+" ");
+		System.out.print(s.pop()+" ");
+		s.push(4);
+		s.push(5);
+		System.out.print(s.pop()+" ");
+		System.out.print(s.pop()+" ");
+		System.out.print(s.pop()+" ");
+		
+	}
+
+}
