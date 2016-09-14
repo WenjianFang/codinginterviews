db0a2ff35068a34f5287a7205dcb5bc06c237feb
wenjian <wenjian@onos6-134.edu.cn>
2016-09-14 10:42:33 +0800

    add judge a sequence is a BST's postOrder seq problem

---
+++ VerifySeqOfBinarySearchTree/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] sequence = {1,2,3,5,4};
+		boolean ret = new Solution().verifySquenceOfBST(sequence);
+		System.out.println(ret);
+	}
+
+}
