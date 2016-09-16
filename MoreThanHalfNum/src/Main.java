495e763ad2e10a3d06eec8aae5d14ad4b1baa4cd
wenjian <wenjian@onos6-134.edu.cn>
2016-09-16 15:28:07 +0800

    add number of array that occur times more than array.length problem

---
+++ MoreThanHalfNum/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {1,2,2,2,3,3,3,3,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,23,4,3,232,2,2,2,2,2,3,4,5,6,7};
+		int ret = new Solution().findMoreThanHalfWithPartition(array);
+		System.out.println(ret);
+	}
+
+}
