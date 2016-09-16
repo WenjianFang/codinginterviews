903bfb785b5229296f03bf19a11428b22c8f8975
wenjian <wenjian@onos6-134.edu.cn>
2016-09-16 21:25:14 +0800

    add largetst sum of continues subArray problem

---
+++ GreatestSumOfSubArray/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {1,-2,3,10,-4,7,2,-5};
+		int ret = new Solution().findGreatestSumOfSubArray(array);
+		System.out.println(ret);
+	}
+
+}
