08ab8b3c878937f0daa073729b830fbdc37e923d
wenjian <wenjian@onos6-134.edu.cn>
2016-09-17 22:01:10 +0800

    add Get continuous array with given sum problem

---
+++ ContinuousNumWithSum/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubArrayList<E>m = 15;
+		int sum  = 100;
+		ArrayList<ArrayList<Integer>> ret = new Solution().getContinuousArrayWithSum(sum);
+		System.out.println(ret.toString());
+	}
+
+}
