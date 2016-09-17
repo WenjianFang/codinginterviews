fab223128ca769a7a3315fafe71310b787be5ad4
wenjian <wenjian@onos6-134.edu.cn>
2016-09-17 11:17:01 +0800

    add get first not repeating char problem

---
+++ FirstNotRepeatingChar/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "aaaaaaa";
+		int ret = new Solution().getIndexOfFirstNotRepeatingChar(str);
+		System.out.println(ret);
+	}
+
+}
